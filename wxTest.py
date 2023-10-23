4995d2e8b4157d3a98d7d7ff5d3e5d923acdae31
ZenthWolf <ZenthRelin@gmail.com>
2023-10-23 19:06:43 -0400

    Simple GUI Elements

--- wxTest.py
+++ wxTest.py
@@ -4,6 +4,14 @@ class myFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         self.panel = wx.Panel(self, wx.ID_ANY)
+        wx.StaticText(self.panel, wx.ID_ANY, "Some static text", pos=(5,60))
+        self.b_test = wx.Button(self.panel, wx.ID_ANY, "Testing", pos=(60,20))
+        self.Bind(wx.EVT_BUTTON, self.on_test, self.b_test)
+    
+    def on_test(self, event):
+        print("Button pressed")
+        event.Skip()
+
 
 class myApp(wx.App):
     def OnInit(self):
