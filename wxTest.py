f8b51d49eeeca14557c43498143ef390fd0e7b90
ZenthWolf <ZenthRelin@gmail.com>
2023-10-23 19:13:47 -0400

    Button Toggling

--- wxTest.py
+++ wxTest.py
@@ -4,12 +4,28 @@ class myFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         self.panel = wx.Panel(self, wx.ID_ANY)
+        
         wx.StaticText(self.panel, wx.ID_ANY, "Some static text", pos=(5,60))
+        
         self.b_test = wx.Button(self.panel, wx.ID_ANY, "Testing", pos=(60,20))
         self.Bind(wx.EVT_BUTTON, self.on_test, self.b_test)
+        
+        self.b_state = wx.Button(self.panel, wx.ID_ANY, "Disable", pos=(160,20))
+        self.Bind(wx.EVT_BUTTON, self.on_state, self.b_state)
     
     def on_test(self, event):
         print("Button pressed")
+        event.Skip()
+    
+    def on_state(self, event):
+        if self.b_state.GetLabel()=="Disable":
+            self.b_test.Disable()
+            self.b_state.SetLabel("Enable")
+        
+        else:
+            self.b_test.Enable()
+            self.b_state.SetLabel("Disable")
+        
         event.Skip()
 
 
