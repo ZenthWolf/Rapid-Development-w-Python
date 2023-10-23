040188e2a81670cb86cce84b23250598b1f81d1b
ZenthWolf <ZenthRelin@gmail.com>
2023-10-23 19:58:12 -0400

    Text Entry Styles

--- wxTest.py
+++ wxTest.py
@@ -5,8 +5,6 @@ class myFrame(wx.Frame):
         wx.Frame.__init__(self, *args, **kwds)
         self.panel = wx.Panel(self, wx.ID_ANY)
         
-        wx.StaticText(self.panel, wx.ID_ANY, "Some static text", pos=(5,60))
-        
         self.b_test = wx.Button(self.panel, wx.ID_ANY, "Testing", pos=(60,20))
         self.Bind(wx.EVT_BUTTON, self.on_test, self.b_test)
         
@@ -17,8 +15,8 @@ class myFrame(wx.Frame):
 #        self.cb_discount = wx.CheckBox(self.panel, wx.ID_ANY, "Apply Discount", \
 #                                       pos = (5, 100))
         #Radio buttons as single declaration
-        self.rb_planet = wx.RadioBox(self.panel, wx.ID_ANY, label="Planets", \
-                                     pos = (60, 100), choices=['Venus', 'Earth', 'Mars'])
+#        self.rb_planet = wx.RadioBox(self.panel, wx.ID_ANY, label="Planets", \
+#                                     pos = (60, 100), choices=['Venus', 'Earth', 'Mars'])
         #Radio buttons as separate declarions
         #Even the demonstration didn't work on this one
 #        self.rb_venus = wx.RadioButton(self.panel, wx.ID_ANY, label="Venus",\
@@ -27,20 +25,25 @@ class myFrame(wx.Frame):
 #                                       pos=(133,100), style=wx.RB_SINGLE)
 #        self.rb_mars = wx.RadioButton(self.panel, wx.ID_ANY, label="Mars",\
 #                                       pos=(199,100), style=wx.RB_SINGLE)
-    
-    def on_test(self, event):
-        #On Press, print set message
-#        print("Button pressed")
 
         #On Press, print message from check box
 #        print(f"Checkbox {self.cb_discount.IsChecked()}")
         #On Press, print message from radio box selection
-        print(self.rb_planet.GetStringSelection())
+#        print(self.rb_planet.GetStringSelection())
         #On Press, print message from single radio buttons
 #        if self.rb_venus.GetValue(): print("Venutian Skies")
 #        if self.rb_earth.GetValue(): print("Welcome to Earth")
 #        if self.rb_mars.GetValue():  print("Martian Dream")
-
+        self.label = wx.StaticText(self.panel, wx.ID_ANY, "Enter Password: ", pos=(60,100))
+        self.tc_pwd = wx.TextCtrl(self.panel, wx.ID_ANY, "", pos=(175,95), \
+                                  size=(100,-1), style=wx.TE_PASSWORD)
+        #Multiline entry
+        #self.tc_test = wx.TextCtrl(self.panel, wx.ID_ANY, "", pos=(60,100), \
+        #    size=(200,120), style=wx.TE_MULTILINE)
+    
+    def on_test(self, event):
+        #On Press, print set message
+        print(self.tc_pwd.GetValue()) #Note: Slightly insecure handling of fictional password is not endorsed by the creator of this code
         event.Skip()
     
     def on_state(self, event):
