ab713ba79365b470c4181ff9f6de3451c00bdde7
ZenthWolf <ZenthRelin@gmail.com>
2023-10-24 00:04:59 -0400

    MenuBar Handling

--- wxTest.py
+++ wxTest.py
@@ -1,4 +1,5 @@
 
+import sys
 import wx
 
 class myFrame(wx.Frame):
@@ -17,6 +18,44 @@ class myFrame(wx.Frame):
                                   pos=(270,20), style=wx.CB_SORT)
         self.cb_test.SetSelection(0)
         self.Bind(wx.EVT_COMBOBOX, self.on_select, self.cb_test)
+
+        menubar = wx.MenuBar()
+        menu_1 = wx.Menu()
+        m_new = menu_1.Append(wx.ID_ANY, "New")
+        m_open = menu_1.Append(wx.ID_ANY, "Open")
+        m_recent = menu_1.Append(wx.ID_ANY, "Recent")
+        m_quit = menu_1.Append(wx.ID_ANY, "Quit\tCTRL+Q")
+        menubar.Append(menu_1, "File")
+
+        menu_2 = wx.Menu()
+        m_help = menu_2.Append(wx.ID_ANY, "Help\tF1")
+        m_about = menu_2.Append(wx.ID_ANY, "About")
+        menubar.Append(menu_2, "Help")
+        self.SetMenuBar(menubar)
+        self.Bind(wx.EVT_MENU, self.on_new, m_new)
+        self.Bind(wx.EVT_MENU, self.on_open, m_open)
+        self.Bind(wx.EVT_MENU, self.on_recent, m_recent)
+        self.Bind(wx.EVT_MENU, self.on_quit, m_quit)
+        self.Bind(wx.EVT_MENU, self.on_help, m_help)
+        self.Bind(wx.EVT_MENU, self.on_about, m_about)
+
+    def on_new(self, event):
+        print("New menu item")
+        event.Skip()
+    def on_open(self, event):
+        print("Open menu item")
+        event.Skip()
+    def on_recent(self, event):
+        print("Recent menu item")
+        event.Skip()
+    def on_quit(self, event):
+        sys.exit(0)
+    def on_help(self, event):
+        print("Help menu item")
+        event.Skip()
+    def on_about(self, event):
+        wx.MessageBox('wxPython Menu Demo', 'Info', wx.OK)
+        event.Skip()
     
     def on_test(self, event):
         #On Press, print set message
